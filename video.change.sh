5eb0fc97abfe54fa3a169eb0c81c6e20a4ce4482
mmfei <wlfkongl@163.com>
2016-11-10 15:35:26 +0800

    add file video.change.sh

---
+++ video.change.sh
@@ -0,0 +1,34 @@
+# ffmpeg -i o.MP4 -s 800x600 o.o.mp4
+if [[ "$#" -lt "3" ]]; then
+    echo "/bin/bash "$0" souredirpath targetdirpath 800x600"
+    exit
+fi
+
+dir=$1;
+todir=$2;
+changefenbianlv=$3;
+if [[ ! -d "$dir" ]]; then
+    exit;
+fi
+if [[ ! -d "$todir" ]]; then
+    mkdir -p $todir;
+fi
+scriptname="$dir/tmp.tmp.sh"
+if [[ -f "$scriptname" ]]; then
+    rm $scriptname;
+fi
+for name in `ls $dir`
+do
+   echo " echo 'changing $name to $changefenbianlv'; " >> $scriptname; 
+   echo " ffmpeg -i $dir/$name -s $changefenbianlv $todir/$changefenbianlv.$name.mp4" >> $scriptname; 
+done
+
+if [[ -f "$scriptname" ]]; then
+    sh $scriptname;
+    #rm $scriptname;
+fi
+
+echo "path $todir";
+ls $todir;
+echo "successful";
+
